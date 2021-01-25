f8b60b822d1dcf3c135a845213f469082fa56a2d
Luiz Sanches <luiz.sanches8910@gmail.com>
2021-01-25 16:29:49 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Dias Cosméticos</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="style.css">
+
+    </head>
+    <body>
+        <div class="container d-none d-sm-block">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <img src="barra.png" alt="" style="margin-top: 9%; margin-left: 1%;">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <img src="title.png" class="" >
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <a href="https://instagram.com">
+                            <img src="ig.png" class="insta">
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <img src="barra.png" alt="" style="margin-top: 2%; margin-left: 1%;">
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <a href="https://api.whatsapp.com/send?phone=15991631400" class="whats">
+                            <img src="whats.png">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container d-block d-sm-none">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <img src="barra.png" alt="" style="margin-top: 20%; margin-left: 1%; width: 100%;">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <img src="title.png" class="title-mob" >
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <a href="https://instagram.com">
+                            <img src="ig.png" class="insta-mob">
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <img src="barra.png" alt="" style="margin-top: 2%; margin-left: 1%; width: 100%;">
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <a href="https://api.whatsapp.com/send?phone=15991631400" class="whats">
+                            <img src="whats.png" class="whats-mob">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <script src="script.js"></script>
+    </body>
+</html>
